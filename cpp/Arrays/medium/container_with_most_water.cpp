7577811fd6e7d9ac12f806bf0619d37a2ba35df4
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-26 23:04:29 +0530

    Add two pointers based solution for container with most water

---
+++ cpp/Arrays/medium/container_with_most_water.cpp
@@ -0,0 +1,15 @@
+/**
+Problem: Container with most water
+LeetCode: 11
+Pattern: Two pointers - array
+Difficulty: Medium
+
+Approach 1: Brute Force - 2 nested loops
+Time: O(n^2)
+Space: O(1)
+
+
+Approach 3: Optimal Two Pointers
+Time: O(n)
+Space: O(1)
+ */
