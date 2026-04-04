f4270ea2767b7d59c459be31f82f3e3bd6c3805b
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-04-04 17:55:46 +0530

    Add Binary Search based solution for Painter's Partition  problem

---
+++ cpp/Binary-Search/hard/aggressive_cows_problem.cpp
@@ -0,0 +1,16 @@
+/**
+ * Problem: Book Allocation Problem
+ * Difficulty: Hard
+ * Pattern: Binary Search
+ *
+ * Approach 1: Optimal  - Binary search
+ * TC = O(n log n)
+ */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
