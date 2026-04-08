18d1a6a30268766bb3a6353a20d9095aef69550d
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-04-08 09:12:08 +0530

    Refactor code structure for improved readability and maintainability

--- cpp/Sorting/sorting_algorithms/insertion_sort.cpp
+++ cpp/Sorting/sorting_algorithms/insertion_sort.cpp
@@ -1,11 +1,41 @@
 /**
- * 
+ *
  */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+void insertionSort(int arr[], int n) // O(n^2)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i - 1;
+        while (prev >= 0 && arr[prev] > curr)
+        {
+            arr[prev + 1] = arr[prev];
+            prev--;
+        }
+        arr[prev + 1] = curr; // placing the correct element in its correct position
+    }
+}
+
+void printArry(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {4, 1, 5, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printArry(arr, n);
+    insertionSort(arr, n);
+    printArry(arr, n);
     return 0;
 }
