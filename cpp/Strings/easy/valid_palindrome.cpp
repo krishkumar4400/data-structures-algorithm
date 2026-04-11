1bd3e2e199e81a7d072984aae06a75155dc6abdb
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-04-11 22:41:07 +0530

    Refactor code structure for improved readability and maintainability

---
+++ cpp/Strings/easy/valid_palindrome.cpp
@@ -0,0 +1,60 @@
+/**
+ * Problem: Find First and Last Position of Element in Sorted Array
+ * Leetcode: 125
+ * Difficulty: Medium
+ * Pattern: Binary Search
+ *
+
+ * Approach 1: Optimal  - Binary search
+ * TC = O(n)
+ * SC = O(log n)
+ */
+
+#include <iostream>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+bool isAlphaNum(char ch)
+{
+    if ((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z'))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isPalindrome(string s)
+{
+    int st = 0;
+    int end = s.length() - 1;
+
+    while (st <= end)
+    {
+        if (!(isAlphaNum(s[st])))
+        {
+            st++;
+            continue;
+        }
+        if (!isAlphaNum(s[end]))
+        {
+            end--;
+            continue;
+        }
+
+        if (tolower(s[st]) != tolower(s[end]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string s = "A man, a plan, a canal: Panama";
+    bool ans = isPalindrome(s);
+    cout << "Ans = " << ans << endl;
+    return 0;
+}
